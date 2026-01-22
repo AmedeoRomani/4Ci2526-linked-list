1924ed921baffa9c9d15f3ef316e50a7295b4df3
Amedeo <amedeo.romani@marconirovereto.it>
2026-01-22 10:48:10 +0100

    Refactor CircularList methods for improved readability and consistency; adjust formatting and logic in checkRing, removeRange, cut, prev, and newRing methods

--- src/CircularList.java
+++ src/CircularList.java
@@ -2,9 +2,8 @@ public class CircularList {
     private Node head;
     private Node last;
 
-   // private int value;
+    // private int value;
 
-    
     public CircularList(Node head, Node last) {
         this.head = head;
         this.last = last;
@@ -22,43 +21,46 @@ public class CircularList {
             coursore.getNext();
         }
     }
-    public boolean checkRing(){
+
+    public boolean checkRing() {
         Node coursore = head;
         if (coursore == null)
             return false;
-        while(true){
-            
-             if(coursore == last)
+        while (true) {
+
+            if (coursore == last)
                 return true;
-            
+
             coursore.getNext();
         }
     }
-    public int removeRange(int start, int end){
+
+    public int removeRange(int start, int end) {
         int sottrazione = start - end;
         Node cursor = head;
-        int contatore =0;
+        int contatore = 0;
         if (head == null)
-            return contatore; 
-        while(true){
+            return contatore;
+        while (true) {
             cursor.getNext();
-            if (contatore == start -1){
-                for (int i =0;i < sottrazione; i++){
+            if (contatore == start - 1) {
+                for (int i = 0; i < sottrazione; i++) {
                     cursor.setNext(cursor.getNext());
                 }
                 return contatore;
             }
         }
     }
-    public void cut(int index){
-        if (index ==0){
+
+    public void cut(int index) {
+        if (index == 0) {
             head = last;
         }
         Node cursor = head;
-        int contatore =0;
+        int contatore = 0;
 
-        while (true){
-            if ( contatore == index){
+        while (true) {
+            if (contatore == index) {
                 cursor = last;
             }
             contatore++;
@@ -66,20 +68,45 @@ public class CircularList {
         }
     }
 
-    public Node prev(Node n){
+    public Node prev(Node n) {
         Node cursor = head;
-        while (true){
-            if (cursor.getNext().equals(n)){
+        while (true) {
+            if (cursor.getNext().equals(n)) {
                 return cursor;
             }
         }
     }
-    public void newRing(Node n1, Node n2){
-        if (n1 == null && n2 == null){
+
+    public void newRing(Node n1, Node n2) {
+        if (n1 == null && n2 == null) {
             n1.setNext(head);
             this.head = n1;
             last.setNext(n2);
             n2 = last;
+        }
+        if (n1 != null && n2 == null) {
+            Node cursor = head;
+            while (cursor == n1) {
+                if (cursor == n1) {
+                    n1 = head;
+                    last.setNext(n2);
+                    n2 = last;
+                    return;
+                }
+                cursor.getNext();
+            }
+        }
+        if (n1 != null && n2 != null) {
+            Node cursor = head;
+            while (cursor == n1) {
+                if (cursor == n1) {
+                    n1 = head;
+                    last.setNext(n2);
+                    n2 = last;
+                    return;
+                }
+                cursor.getNext();
+            }
+        }
     }
-    
-    }
+}
